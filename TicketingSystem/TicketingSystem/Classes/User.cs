b0dbc079846c6721c3efc98c616bcaca09785b05
Alex <109491531+ilexl@users.noreply.github.com>
2023-05-25 16:00:38 +1200

    Testing

--- TicketingSystem/TicketingSystem/Classes/User.cs
+++ TicketingSystem/TicketingSystem/Classes/User.cs
@@ -8,5 +8,6 @@ namespace TicketingSystem.Classes
 {
     class User
     {
+
     }
 }
--- TicketingSystem/TicketingSystem/MainWindow.xaml.cs
+++ TicketingSystem/TicketingSystem/MainWindow.xaml.cs
@@ -15,14 +15,85 @@ using System.Windows.Shapes;
 
 namespace TicketingSystem
 {
+    
+    
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool LoggedIn = false; // temp
         public MainWindow()
         {
             InitializeComponent();
+
+            if (LoggedIn)
+            {
+                MainWindowVisability(true);
+                LoginWindowVisability(false);
+            }
+            else
+            {
+                MainWindowVisability(false);
+                LoginWindowVisability(true);
+            }
+
+
         }
+
+        private void ChangeWindow(string windowName)
+        {
+            mainFrame.Navigate(new Uri("./Frames/" + windowName, UriKind.Relative));
+        }
+
+        /// <summary>
+        /// Hides or Shows the main window and nav bar
+        /// </summary>
+        /// <param name="shown"></param>
+        private void MainWindowVisability(bool shown)
+        {
+            if (shown)
+            {
+                ContentHolder.Visibility = Visibility.Visible;
+                SideBarHolder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ContentHolder.Visibility = Visibility.Hidden;
+                SideBarHolder.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void LoginWindowVisability(bool shown)
+        {
+            if (shown)
+            {
+                Login.Visibility =  Visibility.Visible;
+                Login.Navigate(new Uri("./Frames/Login.xaml", UriKind.Relative));
+            }
+            else
+            {
+                Login.Visibility = Visibility.Hidden;
+            }
+        }
+
+        public void LoginActivation()
+        {
+            MainWindowVisability(true);
+            LoginWindowVisability(false);
+            ChangeWindow("Dashboard.xaml");
+        }
+
+        public void Button_Dashboard(object sender, RoutedEventArgs e)
+        {
+            ChangeWindow("Dashboard.xaml");
+        }
+        public void Button_Settings(object sender, RoutedEventArgs e)
+        {
+            ChangeWindow("Settings.xaml");
+        }
+
+
     }
 }
