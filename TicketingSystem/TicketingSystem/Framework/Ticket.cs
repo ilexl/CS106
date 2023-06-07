866b48717747049ac510a7c686c92c3362429a9c
Alex <109491531+ilexl@users.noreply.github.com>
2023-06-07 13:35:00 +1200

    nav bar fix
    now loads correct buttons based on user type

--- TicketingSystem/TicketingSystem/Framework/Ticket.cs
+++ TicketingSystem/TicketingSystem/Framework/Ticket.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TicketingSystem.Classes
+namespace TicketingSystem.Framework
 {
     class Ticket
     {
---
+++ TicketingSystem/TicketingSystem/Framework/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketingSystem.Framework
+{
+    public class User
+    {
+        public enum Type : int
+        {
+            User = 1,
+            Tech = 2,
+            Admin = 3,
+            Test = 4
+        }
+    }
+}
