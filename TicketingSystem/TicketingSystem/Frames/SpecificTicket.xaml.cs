679adac5d8812a766c036dc44dbfc8a49e67bc80
AdmiralFjalar <oliver@gronkrans.com>
2023-06-15 19:29:50 +1200

    Added comment backend
    * Comments are now loaded in and added to ticket view
    * Users can now add comments

--- TicketingSystem/TicketingSystem/Frames/SpecificTicket.xaml.cs
+++ TicketingSystem/TicketingSystem/Frames/SpecificTicket.xaml.cs
@@ -71,6 +71,127 @@ namespace TicketingSystem.Frames
             {
                 Updated.Content = UpdatedDW.Minutes + "m ago";
             }
+
+            List<String> comments = t.GetComments();
+
+            foreach (string comment in comments)
+            {
+                Grid newCommentGrid = new Grid();
+                newCommentGrid.Margin = new Thickness(0, 0, 0, 20);
+                newCommentGrid.Height = 100;
+                newCommentGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                ColumnDefinition columnDefinition = new ColumnDefinition();
+                columnDefinition.Width = new GridLength(8, GridUnitType.Star);
+                newCommentGrid.ColumnDefinitions.Add(columnDefinition);
+                
+                TextBlock commentInitials = new TextBlock();
+                commentInitials.Text = comment.Substring(0, 2);
+                commentInitials.FontSize = 50;
+                commentInitials.Width = 100;
+                //commentInitials.VerticalContentAlignment = VerticalAlignment.Center;
+                //commentInitials.HorizontalContentAlignment = HorizontalAlignment.Center;
+                Border roundedCorner = new Border();
+                roundedCorner.Background = Brushes.White;
+                roundedCorner.Width = 100;
+                roundedCorner.Padding = new Thickness(17, 12, 0, 0);
+                roundedCorner.CornerRadius = new CornerRadius(60);
+                roundedCorner.Child = commentInitials;
+
+                newCommentGrid.Children.Add(roundedCorner);
+
+                Line line = new Line();
+                line.VerticalAlignment = VerticalAlignment.Center;
+                line.Margin = new Thickness(15, 15, 0, 0);
+                line.StrokeEndLineCap = PenLineCap.Triangle;
+                line.StrokeStartLineCap = PenLineCap.Round;
+                line.StrokeThickness = 80;
+                line.Stroke = Brushes.White;
+                line.X2 = 20;
+                line.Y2 = 20;
+                line.X1 = 1020;
+                line.Y1 = 20;
+
+                newCommentGrid.Children.Add(line);
+                line.SetValue(Grid.ColumnProperty, 1);
+
+                StackPanel stackPanel = new StackPanel();
+                stackPanel.SetValue(Grid.ColumnProperty, 1);
+
+                TextBlock nameBlock = new TextBlock();
+                nameBlock.Text = comment.Substring(0, 2);
+                nameBlock.Margin = new Thickness(40, 10, 0, 0);
+                nameBlock.FontSize = 40;
+                nameBlock.FontWeight = FontWeights.SemiBold;
+                nameBlock.FontFamily = new FontFamily("{DynamicResource Epilogue}");
+
+                stackPanel.Children.Add(nameBlock);
+
+                TextBlock commentBlock = new TextBlock();
+                commentBlock.Text = comment.Substring(2);
+                commentBlock.Margin = new Thickness(40, 0, 0, 0);
+                commentBlock.FontSize = 25;
+                commentBlock.FontWeight = FontWeights.Regular;
+                commentBlock.FontFamily = new FontFamily("{DynamicResource Epilogue}");
+
+                stackPanel.Children.Add(commentBlock);
+
+                newCommentGrid.Children.Add(stackPanel);
+
+                CommentField.Children.Add(newCommentGrid);
+            }
+        }
+
+        private void ResetText()
+        {
+            CommentInputField.Text = "Add Comment Here";
+            CommentInputField.Foreground = Brushes.Gray;
+            CommentField.Children.Clear();
+            CommentButton.Focus();
+            ViewTicketDetails(target);
+        }
+
+        private void CommentButton_Click(object sender, RoutedEventArgs e)
+        {
+            target.AddComment(CommentInputField.Text);
+            ResetText();
+        }
+
+        private void CommentInputField_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Add Comment Here")
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = Brushes.Black; // Set the desired text color
+            }
+        }
+
+        private void CommentInputField_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Text = "Add Comment Here";
+                textBox.Foreground = Brushes.Gray; // Set the desired ghost text color
+            }
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return || e.Key == Key.Enter)
+            {
+                if (CommentInputField.Text == string.Empty || CommentInputField.Text == "Add Comment Here")
+                {
+                    ResetText();
+                    MessageBoxResult emptyComment = MessageBox.Show("Fill out comment before submitting!");
+                }
+                else
+                {
+                    target.AddComment(CommentInputField.Text);
+                    ResetText();
+                }
+            }
         }
     }
 }
