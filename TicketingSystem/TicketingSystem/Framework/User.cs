655a4dc718718dedf70592907d16344f0c987372
AdmFjalar <oliver@gronkrans.com>
2023-06-19 15:40:35 +1200

    Fixed login
    * Users can now login with either email or ID

--- TicketingSystem/TicketingSystem/Framework/User.cs
+++ TicketingSystem/TicketingSystem/Framework/User.cs
@@ -85,10 +85,12 @@ namespace TicketingSystem.Framework
                 SqlCommand command = new SqlCommand();      //  USED TO SPECIFY THE SQL QUERY
 
                 command.Connection = connection;            //  SPECIFIES THE CONNECTION THAT THE COMMAND WILL BE USED IN
-                command.CommandText = "SELECT * FROM Users WHERE (ID=@id OR Email=@id) AND Password=@password;";
+                command.CommandText = "SELECT * FROM Users WHERE Password=@password AND Email=@email;";
                 command.Parameters.AddWithValue("@id", _ID); //  ADDS THE ID TO THE QUERY
+                command.Parameters.AddWithValue("@email", _ID); //  ADDS THE ID TO THE QUERY
                 command.Parameters.AddWithValue("@password", _Password); //  ADDS THE PASSWORD TO THE QUERY
                 sqlReader = command.ExecuteReader();        //  TAKES THE OUTPUT INTO THE READER
+
 
                 if (sqlReader.HasRows) //  USER FOUND WITH MATCHING CREDENTIALS
                 {
@@ -107,8 +109,30 @@ namespace TicketingSystem.Framework
                 }
                 else //  INCORRECT / INVALID CREDENTIALS
                 {
-                    Server.CloseConnection(sqlReader, command, connection);
-                    return false;
+                    sqlReader.Close();
+                    command.CommandText = "SELECT * FROM Users WHERE Password=@password AND ID=@id;";
+                    sqlReader = command.ExecuteReader();        //  TAKES THE OUTPUT INTO THE READER
+
+                    if (sqlReader.HasRows) //  USER FOUND WITH MATCHING CREDENTIALS
+                    {
+                        while (sqlReader.Read())
+                        {
+                            ID = sqlReader.GetInt32(0);         //  Sets this instance's ID to the the corresponding cell in the matching row
+                            password = sqlReader.GetString(1);  //  Sets this instance's password to the the corresponding cell in the matching row
+                            userType = sqlReader.GetInt32(2);   //  Sets this instance's usertype to the the corresponding cell in the matching row
+                            email = sqlReader.GetString(3);     //  Sets this instance's email to the the corresponding cell in the matching row
+                            firstName = sqlReader.GetString(4); //  Sets this instance's first name to the the corresponding cell in the matching row
+                            lastName = sqlReader.GetString(5);  //  Sets this instance's last name to the the corresponding cell in the matching row
+
+                            Server.CloseConnection(sqlReader, command, connection);
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        Server.CloseConnection(sqlReader, command, connection);
+                        return false;
+                    }
                 }
                 return false;
             }
